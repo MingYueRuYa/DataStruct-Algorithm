9eee9922f22d2dcb7379ede14f0576f5d22e14d7
Shixiong Liu 刘世雄 <635672377@qq.com>
2021-07-30 17:02:17 +0800

    add graph shortest path file not contain code.

---
+++ algorithm_datastruct/graph_shortest_path.h
@@ -0,0 +1,10 @@
+#ifndef graph_shortest_path_h
+#define graph_shortest_path_h
+
+
+// 1.使用Dijkstra算法求最短路径
+// (1) 在图中以任一点为起始点start
+// (2) 遍历与start关联的所有顶点，并将对应的权值放到最小索引堆中。并将遍历过的顶点设置为true，表示已经遍历过
+// (3) 从最小索引堆中弹出权值最小的点
+
+#endif // graph_shortest_path_h
